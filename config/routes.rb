62999eae46b17dc15c5966bf82f977af95ffad7e
Dhruvika Kevat <dhruvika.k@simformsolutions.com>
2023-01-31 10:25:49 +0530

    CRUD-app

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  # get 'posts/index'
+  # get 'posts/show'
+  # get 'posts/new'
+  # get 'posts/edit'
+
+  resources :posts
+  root 'posts#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
